e303050503e504a822048a1a7e14705ba304feb5
Syuq <tatd.wine@gmail.com>
2022-01-20 14:21:49 +0700

    chore: refactor dwm config

--- config.h
+++ config.h
@@ -15,7 +15,8 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true"  };
+/* static char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true"  }; */
+static char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
