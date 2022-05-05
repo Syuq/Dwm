bca64b81ee87566259733796257d8a2fac2470e7
Syuq <tatd.wine@gmail.com>
2022-05-05 14:14:00 +0700

    chore: modified config.h dwm

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static unsigned int gappoh    = 7;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 /* static char *fonts[]          = { "JetBrainsMono Nerd Font:size=10", "JetBrainsMono Nerd Font:pixelsize=10:antialias=true:autohint=true"  }; */
