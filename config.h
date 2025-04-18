04ec2e5e04a2f443c7b72da78f75b81098a8701d
Syuq <tatd.wine@gmail.com>
2025-04-18 16:56:50 +0700

    chore(config): adjust window gaps and add new keybinding for app launcher Reduced outer and inner gaps (`gappoh`, `borderpx`) to create a more compact window layout, which improves screen real estate usage. Enabled `smartgaps` so outer gaps are removed when only one window is open, enhancing focus and minimalism.
    Also added a new keybinding (`MODKEY + Menu`) to launch `rofi` in application mode
    for quicker access to installed apps, improving usability.

--- config.h
+++ config.h
@@ -6,18 +6,18 @@
 #define BROWSER "brave"
 
 /* appearance */
-static unsigned int borderpx = 3; /* border pixel of windows */
+static unsigned int borderpx = 1; /* border pixel of windows */
 static unsigned int snap = 32;    /* snap pixel */
 static unsigned int gappih = 20;  /* horiz inner gap between windows */
 static unsigned int gappiv = 10;  /* vert inner gap between windows */
 static unsigned int gappoh =
-    10; /* horiz outer gap between windows and screen edge */
+    7; /* horiz outer gap between windows and screen edge */
 static unsigned int gappov =
     30; /* vert outer gap between windows and screen edge */
 static int swallowfloating =
     0; /* 1 means swallow floating windows by default */
 static int smartgaps =
-    0;                  /* 1 means no outer gap when there is only one window */
+    1;                  /* 1 means no outer gap when there is only one window */
 static int showbar = 1; /* 0 means no bar */
 static int topbar = 1;  /* 0 means bottom bar */
 static char *fonts[] = {
@@ -273,6 +273,7 @@ static const Key keys[] = {
      * },
      */
     {MODKEY, XK_c, spawn, SHCMD("clipcat-menu")},
+    {MODKEY, XK_Menu, spawn, SHCMD("rofi -modi drun -show drun")},
     // {MODKEY, XK_c, spawn,
     //  SHCMD("rofi -modi 'clipboard:greenclip print' -show clipboard
     //  -run-command '{cmd}'")},
